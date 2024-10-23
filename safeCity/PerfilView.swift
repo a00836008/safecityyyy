4a75cc34af23a3a97808048d3ace2b07098f0925
ElingeMisa <a01741176@tec.mx>
2024-10-23 11:23:07 -0600

    PerfilView: Creado y con los placeholders logInView: no puse nada , pero aparece como que se cambio algo

---
+++ safeCity/PerfilView.swift
@@ -0,0 +1,18 @@
+//
+//  PerfilView.swift
+//  safeCity
+//
+//  Created by Victor Misael Escalante Alvarado on 23/10/24.
+//
+
+import SwiftUI
+
+struct PerfilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PerfilView()
+}
